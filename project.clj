c1beaeebc867b24a0e6df77266e390e4b53ae09a
Sébastien Wagener <sebastien.wagener@gmail.com>
2019-07-26 09:03:12 +0200

    Update myhtml4j

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.github.foobar27/myhtml4j "0.1.3"]
+                 [com.github.foobar27/myhtml4j "0.1.7"]
 
                  ;; TODO dev dependency only
                  [org.clojure/test.check "0.9.0"]])
